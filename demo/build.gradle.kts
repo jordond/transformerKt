6c3c4b2fbc342540bf76511ed3f75b0f1f792ed3
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-05-22 19:50:34 -0400

    add required dependencies

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -48,8 +52,16 @@ android {
 dependencies {
     implementation(project(":transformerkt"))
 
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.hilt.binder)
+    ksp(libs.hilt.binder.compiler)
+
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.activity.compose)
 
     implementation(platform(libs.compose.bom))
@@ -59,6 +71,13 @@ dependencies {
     implementation(libs.material3)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.ui)
+    implementation(libs.io.github.raamcosta.compose.destinations.core)
+    ksp(libs.ksp)
+
+    implementation(libs.napier)
 
     testImplementation(libs.junit.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
