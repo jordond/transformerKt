6a6a0ac4d0a660f503a2fadb89fd11bc7acb8d51
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-06-08 20:25:08 -0400

    Feature - Update dependencies (#7)
    - media3 - 1.1.0-beta01
    - AGP - 8.1.0-beta05
    - Dokka - 1.8.20
    - Some demo dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.1.0-beta03" apply false
-    id("com.android.library") version "8.1.0-beta03" apply false
+    id("com.android.application") version "8.1.0-beta05" apply false
+    id("com.android.library") version "8.1.0-beta05" apply false
     id("org.jetbrains.kotlin.android") apply false
     id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
     id("com.google.dagger.hilt.android") apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,5 @@
 @file:Suppress("UnstableApiUsage")
 
-import de.fayard.refreshVersions.core.StabilityLevel
-
 pluginManagement {
     repositories {
         google()
