1a0d8855a96f99f727aad7700b4a34122e15aea0
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-07-13 12:31:48 -0400

    add Poko to generate equals, toString, and hashcode functions

--- transformerkt/src/main/java/dev/transformerkt/TransformerStatus.kt
+++ transformerkt/src/main/java/dev/transformerkt/TransformerStatus.kt
@@ -4,6 +4,7 @@ import androidx.annotation.IntRange
 import androidx.media3.transformer.ExportResult
 import androidx.media3.transformer.TransformationRequest
 import androidx.media3.transformer.Transformer
+import dev.drewhamilton.poko.Poko
 import java.io.File
 
 /**
@@ -21,6 +22,7 @@ public sealed interface TransformerStatus {
      *
      * @param[progress] Integer progress value between 0-100
      */
+    @Poko
     public class Progress(
         @IntRange(from = 0, to = 100)
         public val progress: Int,
@@ -31,6 +33,7 @@ public sealed interface TransformerStatus {
      *
      * @param[output] The output [File] of the [Transformer] execution.
      */
+    @Poko
     public class Success(
         public val output: File,
         public val exportResult: ExportResult,
@@ -41,5 +44,6 @@ public sealed interface TransformerStatus {
      *
      * @param[cause] The [Throwable] that caused the failure.
      */
+    @Poko
     public class Failure(public val cause: Throwable) : TransformerStatus, Finished
 }
