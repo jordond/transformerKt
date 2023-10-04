ff67401b590b529c4b3a2ff0ae6766129a07a1dc
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-10-04 14:07:28 -0400

    add image to video

--- transformerkt/src/main/java/dev/transformerkt/dsl/composition/SequenceBuilder.kt
+++ transformerkt/src/main/java/dev/transformerkt/dsl/composition/SequenceBuilder.kt
@@ -4,6 +4,8 @@ import android.net.Uri
 import androidx.core.net.toUri
 import androidx.media3.common.MediaItem
 import androidx.media3.transformer.EditedMediaItem
+import dev.transformerkt.dsl.effects.EffectsBuilder
+import dev.transformerkt.dsl.effects.setEffects
 import dev.transformerkt.ktx.edited
 import java.io.File
 
@@ -29,25 +31,25 @@ public interface SequenceBuilder {
         uri: Uri,
         durationMs: Long,
         frameRate: Int = 30,
-        block: EditedMediaItem.Builder.() -> Unit = {},
+        block: EffectsBuilder.() -> Unit = {},
     ): EditedMediaItem = item(uri) {
         setDurationUs(durationMs * 1000)
         setFrameRate(frameRate)
-        apply(block)
+        setEffects(block)
     }
 
     public fun image(
         file: File,
         durationMs: Long,
         frameRate: Int = 30,
-        block: EditedMediaItem.Builder.() -> Unit = {},
+        block: EffectsBuilder.() -> Unit = {},
     ): EditedMediaItem = image(file.toUri(), durationMs, frameRate, block)
 
     public fun image(
         path: String,
         durationMs: Long,
         frameRate: Int = 30,
-        block: EditedMediaItem.Builder.() -> Unit = {},
+        block: EffectsBuilder.() -> Unit = {},
     ): EditedMediaItem = image(File(path), durationMs, frameRate, block)
 }
 
